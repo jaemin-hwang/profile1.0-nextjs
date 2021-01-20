29f09e836e5fb14d2bf9185c4ee524352d2f690a
jaemin-hwang <hjm8169@naver.com>
2021-01-20 10:01:21 +0900

    event 추가

--- components/Introduce.js
+++ components/Introduce.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
 import {makeStyles} from "@material-ui/core/styles";
 import profileImage from '../public/assets/PhotoView.jpg';
+import { Fade, Slide } from "react-awesome-reveal";
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -25,7 +26,149 @@ const Introduce = () => {
                     <img src={profileImage} width={"50%"}/>
                 </div>
                 <div>
-                    asdasdasdasdasd
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                </div>
+            </div>
+            <div className={classes.profile}>
+                <div>
+                    <img src={profileImage} width={"50%"}/>
+                </div>
+                <div>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                </div>
+            </div>
+            <Slide direction={"right"} triggerOnce>
+                <div className={classes.profile}>
+                    <div>
+                        <img src={profileImage} width={"50%"}/>
+                    </div>
+                    <div>
+                        asdasdasdasdasd<br/>
+                        asdasdasdasdasd<br/>
+                        asdasdasdasdasd<br/>
+                        asdasdasdasdasd<br/>
+                        asdasdasdasdasd<br/>
+                        asdasdasdasdasd<br/>
+                        asdasdasdasdasd<br/>
+                        asdasdasdasdasd<br/>
+                        asdasdasdasdasd<br/>
+                        asdasdasdasdasd<br/>
+                        asdasdasdasdasd<br/>
+                        asdasdasdasdasd<br/>
+                        asdasdasdasdasd<br/>
+                        asdasdasdasdasd<br/>
+                    </div>
+                </div>
+            </Slide>
+
+            <div className={classes.profile}>
+                <div>
+                    <img src={profileImage} width={"50%"}/>
+                </div>
+                <div>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                </div>
+            </div>
+            <div className={classes.profile}>
+                <div>
+                    <img src={profileImage} width={"50%"}/>
+                </div>
+                <div>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                </div>
+            </div>
+            <div className={classes.profile}>
+                <div>
+                    <img src={profileImage} width={"50%"}/>
+                </div>
+                <div>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                </div>
+            </div>
+            <div className={classes.profile}>
+                <div>
+                    <img src={profileImage} width={"50%"}/>
+                </div>
+                <div>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
+                    asdasdasdasdasd<br/>
                 </div>
             </div>
         </div>
